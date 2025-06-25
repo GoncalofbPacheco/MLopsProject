69294b6e19e8ca74644ec8b4c5d25fa2d79cddeb
GoncalofbPacheco <20240695@novaims.unl.pt>
2025-06-25 23:52:07 +0100

    feature_selection

--- src/mlopsproject/pipeline_registry.py
+++ src/mlopsproject/pipeline_registry.py
@@ -8,6 +8,7 @@ from mlopsproject.pipelines import data_unit_tests as data_unit_tests_pipeline
 from mlopsproject.pipelines import split_data as split_data_pipeline
 from mlopsproject.pipelines import preprocessing_train as preprocess_train_pipeline
 from mlopsproject.pipelines import split_train as split_train_pipeline
+from mlopsproject.pipelines import feature_selection as feature_selection_pipeline
 
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -17,5 +18,6 @@ def register_pipelines() -> dict[str, Pipeline]:
     pipelines["split_data"] = split_data_pipeline.create_pipeline()
     pipelines["preprocessing_train"] = preprocess_train_pipeline.create_pipeline()
     pipelines["split_train"] = split_train_pipeline.create_pipeline()
+    pipelines["feature_selection"] = feature_selection_pipeline.create_pipeline()
     pipelines["__default__"] = sum(pipelines.values())
     return pipelines
