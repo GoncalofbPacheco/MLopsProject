161c69f6e6418e670c109387ffc941fc8c118547
GoncalofbPacheco <20240695@novaims.unl.pt>
2025-06-28 00:23:35 +0100

    .

---
+++ src/mlopsproject/pipelines/preprocessing_batch/pipeline.py
@@ -0,0 +1,21 @@
+"""
+This is a boilerplate pipeline
+generated using Kedro 0.18.8
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import feature_engineer
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=feature_engineer,
+                inputs=["heart_data_ingested", "encoder"],
+                outputs="preprocessed_batch_data",
+                name="preprocessed_batch",
+            ),
+        ]
+    )
