56da5640c74f9a15c4fff842492d3c56c7c822ae
AfonsoG2001 <20240752@novaims.unl.pt>
2025-06-28 17:14:27 +0100

    all runned

--- src/mlopsproject/pipeline_registry.py
+++ src/mlopsproject/pipeline_registry.py
@@ -8,9 +8,9 @@ from mlopsproject.pipelines import (
     data_unit_tests as data_tests,
     preprocessing_train as preprocess_train,
     split_train as split_train,
-    model_selection as model_selection_pipeline,
-    model_train as model_train_pipeline,
-    feature_selection as feature_selection_pipeline,
+    model_selection as model_selection,
+    model_train as model_train,
+    feature_selection as feature_selection,
     split_data,
     preprocessing_batch,
     model_predict,
@@ -29,9 +29,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     split_data_pipeline = split_data.create_pipeline()
     preprocess_train_pipeline = preprocess_train.create_pipeline()
     split_train_pipeline = split_train.create_pipeline()
-    model_train = model_train_pipeline.create_pipeline()
-    model_selection = model_selection_pipeline.create_pipeline()
-    feature_selection = feature_selection_pipeline.create_pipeline()
+    model_train_pipeline = model_train.create_pipeline()
+    model_selection_pipeline = model_selection.create_pipeline()
+    feature_selection_pipeline = feature_selection.create_pipeline()
     preprocess_batch_pipeline = preprocessing_batch.create_pipeline()
     model_predict_pipeline = model_predict.create_pipeline()
     data_drift_pipeline = data_drift.create_pipeline()
@@ -42,15 +42,15 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "split_data": split_data_pipeline,
         "preprocess_train": preprocess_train_pipeline,
         "split_train": split_train_pipeline,
-        "model_selection": model_selection,
-        "model_train": model_train,
-        "feature_selection": feature_selection,
+        "model_selection": model_selection_pipeline,
+        "model_train": model_train_pipeline,
+        "feature_selection": feature_selection_pipeline,
         "production_full_train_process": preprocess_train_pipeline
         + split_train_pipeline
-        + model_train,
+        + model_train_pipeline,
         "preprocess_batch": preprocess_batch_pipeline,
         "inference": model_predict_pipeline,
         "production_full_prediction_process": preprocess_batch_pipeline
         + model_predict_pipeline,
-        "data_drift_pipeline": data_drift_pipeline,
+        "data_drift": data_drift_pipeline,
     }
